bebbcc0f9fee884e752c389eeff46d4483f1671d
apoorvvw <apoorvvw@gmail.com>
2017-07-25 07:51:52 -0500

    Initial check-in of module notificationhub

---
+++ notificationhub/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace notificationhub
+{
+    public class Constants
+    {
+        public Constants()
+        {
+        }
+		// Azure app-specific connection string and hub path
+		public const string ConnectionString = "Endpoint=sb://ns-notificationhubios.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=immMA/de52Pbmo2hVy1x08pWnHd8dKUokMYXRHUx/FI=";
+		public const string NotificationHubPath = "hub_notificationhubiOS";
+    }
+}
